dffa21854332436e5dcec5e96b72ba4ba7deb018
Kaz Namba <kaz.namba@gmail.com>
2018-06-08 17:51:08 +0900

    feat: generateScripts now accepts object

--- __tests__/scripts.js
+++ __tests__/scripts.js
@@ -20,4 +20,33 @@ describe('generateScripts', () => {
         src: '/some-local.min.js'
       }])
   })
+
+  test('works with object', () => {
+    expect(generateScripts([
+      {
+        name: 'some-package',
+        defer: 'defer'
+      }
+    ]))
+      .toEqual([{
+        type: 'text/javascript',
+        src: 'https://unpkg.com/some-package/dist/some-package.js',
+        name: 'some-package',
+        defer: 'defer'
+      }])
+  })
+
+  test('works with object 2', () => {
+    expect(generateScripts([
+      {
+        src: '/some-local.min.js',
+        defer: 'defer'
+      }
+    ]))
+      .toEqual([{
+        type: 'text/javascript',
+        src: '/some-local.min.js',
+        defer: 'defer'
+      }])
+  })
 })
